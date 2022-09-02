9c398951aa6e75021c046dadadb7632d87a8f8e8
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2022-09-02 16:18:13 -0300

    fix styles Botones

--- src/components/contenedores/Contenedor.js
+++ src/components/contenedores/Contenedor.js
@@ -4,10 +4,10 @@ import styled from "styled-components";
     @param direccion
     @param alineacionX
     @param alineacionY
-    @param width
-    @param height
+    @param ancho
+    @param alto
     @param border 
-    
+    @param radio
 */
 
 const Contenedor = styled.div`
@@ -15,10 +15,12 @@ const Contenedor = styled.div`
     flex-direction: ${props => props.direccion ? props.direccion : "row"};
     align-items: ${props => props.alineacionX ? props.alineacionX : "center"};
     justify-content: ${props => props.alineacionY ? props.alineacionY : "center"};
-    width: ${props => props.width ? props.width : "auto"};
-    height: ${props => props.height ? props.height : "auto"};
+    width: ${props => props.ancho ? props.ancho : "50%"};
+    height: ${props => props.alto ? props.alto : "auto"};
     padding: ${props => props.padding ? props.padding : "20px"};
     border: ${props => props.border ? props.border : "none"};
+    border-radius: ${props => props.radio ? props.radio : "0px"};
+    background-color: ${props => props.colorFondo ? props.colorFondo : "initial"};
 `;
 
 export default Contenedor;
