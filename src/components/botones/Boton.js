809aa241749bbecfb86f2040b4e304a913c0af0d
Daniel-Aizcorbe <daizcorbe@frba.utn.edu.ar>
2022-09-02 16:18:13 -0300

    fix styles Botones

--- src/components/botones/Boton.js
+++ src/components/botones/Boton.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
     @param padding
     @param radio
     @param colorFondo
-    @param color
+    @param colorLetra
 */
 
 const Boton = styled.button`
@@ -14,11 +14,13 @@ const Boton = styled.button`
     padding: ${props => props.padding ? props.padding : "16px 32px"};
     border-radius: ${props => props.radio ? props.radio : "25px"};
     border: 1px solid ${props => props.colorFondo ? props.colorFondo : "#2EC4B6"};
-    color: ${props => props.color ? props.color : "black"};
+    color: ${props => props.colorLetra ? props.colorLetra : "black"};
     background-color: ${props => props.colorFondo ? props.colorFondo : "#2EC4B6"};
     cursor: pointer;
     user-select: none;
-
+    font-family: Verdana, sans-serif;
+    transition: .3s ease all;
+  
     &:hover {
         background-color: ${props => props.colorFondo ? props.colorFondo : "#2EC4B6"}80;
     }
